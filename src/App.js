cb3eec4fb9b449e3a72af9af302777a26a3d9478
ebenezergodwyll <ebenezergodwyll@gmail.com>
2022-10-31 07:50:23 +0000

    SIMPLE Card Latest

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import SimpleCard from './SimpleCard';
+
+
+
+
+
 
 function App() {
+  let Title="Welcome to the Chech Republic"
+    let Info="A beautiful country with a lot of lovely natural landscape."
+    let Pic= <img src='https://media.istockphoto.com/photos/adult-male-tourist-with-a-backpack-looking-from-a-viepoint-at-rock-picture-id1324670141?s=612x612' alt={""} ></img> 
+  
+  let FullApp=[Title,Info,Pic]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SimpleCard Complete= {FullApp}/>
     </div>
   );
 }
 
-export default App;
+export default App 
